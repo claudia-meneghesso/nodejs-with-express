9be953bb5c25cb71ce2097dcc5276f65b6159f4a
claudia_meneghesso <claudia.meneghesso@yld.io>
2023-12-22 11:07:43 +0100

    Delete unused folder and add route to orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -29,3 +29,7 @@ exports.getCart = (req, res, next) => {
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", { docTitle: "Checkout", path: "/checkout" });
 };
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", { docTitle: "Orders", path: "/orders" });
+};
